35ca4f97b0f6f18ab57bb24dfda8b79f5aec5305
subham sarkar <subhamsarkar.aeie2021@nsec.ac.in>
2025-03-01 12:58:52 +0530

    updated  lib/api.ts for executing suggest code ... at(#0009)...

--- code-executor-frontend/components/CodeEditor.tsx
+++ code-executor-frontend/components/CodeEditor.tsx
@@ -32,7 +32,7 @@ const CodeEditor=() => {
             {/* Run Button */}
             <button
                 onClick={handleRun}
-                className="mt-4 w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                className="mt-4 w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer"
             >
                 Run
             </button>
--- code-executor-frontend/lib/api.ts
+++ code-executor-frontend/lib/api.ts
@@ -3,6 +3,7 @@
 import axios from "axios";
 
 const API_URL = "http://localhost:3001/execute/code";
+const API_URL1 = "http://localhost:3001/execute/suggest";
 
 export const executeCode =  async (code:string,language:string) => {
     try {
@@ -25,4 +26,24 @@ export const executeCode =  async (code:string,language:string) => {
         return "Execution error";
         
     }
+}
+
+
+export const getCodeSuggestion =  async(language:string,currentCode:string) => {
+    try {
+        const token  =  localStorage.getItem("token")
+        const response  =  await axios.post(API_URL1,{language,currentCode},{
+            headers:{
+                Authorization: `Bearer ${token}`
+            }
+        })
+        return response.data
+
+        
+    } catch (error) {
+        console.error("Error fetching code suggestion:", error);
+        return "Error generating suggestion";
+        
+    }
+
 }
