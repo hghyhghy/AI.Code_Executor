528fc06389bdfcb00ee086041913fa7627ce392b
subham sarkar <subhamsarkar.aeie2021@nsec.ac.in>
2025-03-10 12:58:39 +0530

    prisma fetched the executio history from the  database sql  at (#0064)..

--- code-executor-backend/src/execution-history/execution-history.service.ts
+++ code-executor-backend/src/execution-history/execution-history.service.ts
@@ -1,4 +1,22 @@
+
+import { PrismaService } from 'src/prisma.service';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ExecutionHistoryService {}
+export class ExecutionHistoryService {
+
+    constructor(private prisma:PrismaService){}
+
+    async  getExecutionHistory(userId:number){
+        return await this.prisma.executionHistory.findMany({
+
+            where:{
+                userId:Number(userId)
+            },
+
+            orderBy:{
+                createdAt:'desc'
+            }
+        })
+    }
+}
