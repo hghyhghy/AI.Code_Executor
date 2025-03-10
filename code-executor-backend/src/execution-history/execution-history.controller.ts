4b5f849cd52022e0d5fd7be2817a2cd9983b3331
subham sarkar <subhamsarkar.aeie2021@nsec.ac.in>
2025-03-10 21:33:42 +0530

    code history fetching controller is added via userid at (#0066)..

--- code-executor-backend/src/execution-history/execution-history.controller.ts
+++ code-executor-backend/src/execution-history/execution-history.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+
+import { Controller,Get,Req,UseGuards } from '@nestjs/common';
+import { ExecutionHistoryService } from './execution-history.service';
+import { Request } from 'express';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('execution-history')
-export class ExecutionHistoryController {}
+export class ExecutionHistoryController {
+    constructor(private readonly  executionHistoryController:ExecutionHistoryService){}
+
+   @UseGuards(JwtAuthGuard)
+   @Get("history")
+   async getUserExecutionHistory(@Req() req:Request){
+    const userId =  (req.user as any)?.id
+    return this.executionHistoryController.getExecutionHistory(Number(userId))
+   }
+}
