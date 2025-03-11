fda2255847fe8f7502fcd055e9f3234a3801a070
subham sarkar <subhamsarkar.aeie2021@nsec.ac.in>
2025-03-11 10:42:58 +0530

    prisma delete is  added for the delete all and delete specific file at (#0076)..

--- code-executor-backend/src/execution-history/execution-history.service.ts
+++ code-executor-backend/src/execution-history/execution-history.service.ts
@@ -27,4 +27,23 @@ export class ExecutionHistoryService {
         })
 
     }
+    
+  // Delete ALL Execution History for a user
+    async  deleteExecutionHistory(userId:number){
+        return await this.prisma.executionHistory.deleteMany({
+            where:{
+                userId:Number(userId)
+            }
+        })
+    }
+
+    // Delete a SPECIFIC Execution History entry
+    async deleteSpecificHistiory(entryId:number,userId:number){
+        return  await this.prisma.executionHistory.delete({
+            where:{
+                id:entryId,
+                userId:Number(userId)
+            }
+        })
+    }
 }
