6ba97c4a593c727f72dfaaeb43f83ca1685914d5
subham sarkar <subhamsarkar.aeie2021@nsec.ac.in>
2025-03-19 00:14:10 +0530

    updated (#54) backend part to provide foldername,filename,language and creation time ...

--- code-executor-backend/src/github/github.service.ts
+++ code-executor-backend/src/github/github.service.ts
@@ -2,27 +2,58 @@
 import { Injectable,HttpException,HttpStatus } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import axios from 'axios';
+import { Code } from 'typeorm';
 
 @Injectable()
 export class GithubService {
 
     constructor(private prisma:PrismaService){}
     //fetching all file name from the user  
-    async getUserFiles(userId:number){
+    async getUserFiles(userId: number) {
 
-        return await this.prisma.file.findMany({
-            where:{
-                folder:{
-                    userId
-                }
+         const files =  await this.prisma.file.findMany({
+          where: {
+            folder: {
+              userId,
             },
-            select:{
-                id:true,
-                name:true
-            }
-            
-        })
-    }
+          },
+          select: {
+            id: true,
+            name: true,
+            folder: {
+              select: {
+                name: true, // Folder Name
+              },
+            },
+            // Removed execution property as it does not exist in the type
+          },
+        });
+
+        const filesWithExecution  =await Promise.all(
+            files.map(async(file) => {
+                const execution = await this.prisma.executionHistory.findUnique({
+                    where:{id:file.id,
+                    },
+
+                    select:{
+                        language:true,
+                        createdAt:true
+                    }
+                    
+                });
+
+                return{
+                    id:file.id,
+                    name:file.name,
+                    folder:file.folder,
+                    execution:execution || null
+                }
+
+            })
+        )
+        console.log(filesWithExecution)
+        return filesWithExecution
+     }
 
     async pushFileToGitHub(
         userId:number,
@@ -33,6 +64,7 @@ export class GithubService {
         commitMessage:string
     ) {
         try {
+            console.log("Pushing file:", { userId, pat, owner, repo, filePath, commitMessage });
             const file =  await this.prisma.file.findFirst({
                 where:{
                     name:filePath,
@@ -72,7 +104,7 @@ export class GithubService {
                     Authorization:`Bearer ${pat}`
                 }
             })
-            
+            console.log("file successfully pushed")
             return{message:"File pushed successfully to the github "}
 
         } catch (error) {
